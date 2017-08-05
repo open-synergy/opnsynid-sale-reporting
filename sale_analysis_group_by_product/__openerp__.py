c63b062e16d3809621b4b1cd07f00dd1bb217032
Andhitia Rama <andhitia.r@gmail.com>
2017-08-05 11:22:05 +0700

    [8.0.1.0.0][sale_analysis_group_by_product

---
+++ sale_analysis_group_by_product/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sales Analysis Group By Product",
+    "version": "8.0.1.0.0",
+    "category": "Sales Management",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "reports/sale_report_views.xml",
+    ],
+}
